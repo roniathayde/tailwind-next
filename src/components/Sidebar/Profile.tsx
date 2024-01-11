70d04ad8edba3230b6053652b6112a7defaa275a
Roni Athayde <pontes014@gmail.com>
2024-01-11 16:40:37 -0300

    bk: warning

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -1,4 +1,5 @@
 import { LogOut } from 'lucide-react'
+import { Button } from '../Button'
 
 export function Profile() {
   return (
@@ -17,12 +18,9 @@ export function Profile() {
           pontes014@gmail.com
         </span>
       </div>
-      <button
-        type="button"
-        className="ml-auto truncate rounded-md p-2 hover:bg-zinc-50"
-      >
+      <Button type="button" variant="ghost">
         <LogOut className=" h-5  w-5   text-zinc-500 " />
-      </button>
+      </Button>
     </div>
   )
 }
